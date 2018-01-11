2fe820f10edfc2f80fdc079d6ab2b4109c75edc5
mauro <maurop123@gmail.com>
2018-01-10 23:01:11 -0300

    add v-app

--- template/src/main.js
+++ template/src/main.js
@@ -7,6 +7,10 @@ import App from './App'
 {{#router}}
 import router from './router'
 {{/router}}
+import Vuetify from 'vuetify'
+import 'vuetify/dist/vuetify.min.css'
+
+Vue.use(Vuetify)
 
 Vue.config.productionTip = false
 
